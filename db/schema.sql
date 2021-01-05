d15cc857150c0b68ccdb15618425e48555c54aa5
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-01-05 12:04:23 -0800

    initial db

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the burgers_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database burgers_db and specify it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table burgers.
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(50) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("Spicy Jalepeno")
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("California")
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("Bacon Cheeseburger")
