34f07fa4c5d6c14186033de88ca615fcdbd97994
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-01-07 11:55:38 -0800

    updated comments

--- config/orm.js
+++ config/orm.js
@@ -29,7 +29,6 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
       // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
