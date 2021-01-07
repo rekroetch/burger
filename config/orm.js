93fe797cbd5a353c77b6243922345c30443290a1
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-01-07 11:57:24 -0800

    updated comments, didn't save last

--- config/orm.js
+++ config/orm.js
@@ -2,9 +2,7 @@
 var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
@@ -89,5 +87,5 @@ var orm = {
   },
 }
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
