6d2d5844c8a8264bb73457ce9dc05fc4e77306eb
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-01-09 16:43:42 -0800

    db connection changed for JawsDB - Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "mYsqu!RRe1",
-  database: "burgers_db"
+  user: "zid9ijnnpmacdabt",
+  password: "vdwkp560wzi6abcc",
+  database: "xttve8zfp2rh3f7v"
 });
 
 // Make connection.
